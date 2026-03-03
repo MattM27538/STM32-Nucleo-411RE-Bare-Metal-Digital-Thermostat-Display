0c76905a611f84add20c8ca10a387a1866f71766
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2026-03-02 20:28:52 -0800

    Initial push delay.c source file

---
+++ Src/delay.c
@@ -0,0 +1,64 @@
+#include "delay.h"
+#include <stdint.h>
+
+void setDelayTime(const uint32_t clockCyclesPerMillisecond){
+	SysTick->LOAD = clockCyclesPerMillisecond - 1;
+}
+
+void resetSysTickCounter(){
+	SysTick->VAL = 0;
+}
+
+void setClockSourceToProcessorClock(){
+	SysTick->CTRL |= (1U << 2);
+}
+
+void enableSysTickCounter(){
+	SysTick->CTRL |= (1U << 0);
+}
+
+void runSysTickDelay(const int delayDuration){
+	const uint32_t sysTickTimerCountDownIsComplete = (1U << 16);
+
+	for (int i = 0; i < delayDuration; ++i){
+		//TODO: change hex value to bit shifted value
+//		 while(!(SysTick->CTRL & 0x10000)){}
+		while(!(SysTick->CTRL & sysTickTimerCountDownIsComplete)){}
+	}
+}
+
+void resetSysTickCSRRegister(){
+	SysTick->CTRL = 0;
+}
+
+//TODO: create interface to refactor both delays
+void delayuS(const int numberOfMicroSeconds){
+	const uint32_t clockCyclesPerMillisecond = 16;
+
+	setDelayTime(clockCyclesPerMillisecond);
+
+	resetSysTickCounter();
+
+	setClockSourceToProcessorClock();
+	
+	enableSysTickCounter();
+
+	runSysTickDelay(numberOfMicroSeconds);
+	
+	resetSysTickCSRRegister();
+}
+
+void delay(const int numberOfMilliSeconds){
+	const uint32_t clockCyclesPerMillisecond = 16000;
+	
+	setDelayTime(clockCyclesPerMillisecond);
+
+	resetSysTickCounter();
+
+	setClockSourceToProcessorClock();
+	enableSysTickCounter();
+
+	runSysTickDelay(numberOfMilliSeconds);
+	
+	resetSysTickCSRRegister();
+}
